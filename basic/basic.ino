58b55c133f7c8237a9c02c7422bf17b2400989b2
tharnyelilvannan <tharny2005@outlook.com>
2025-12-16 16:55:09 -0500

    Add IR and US sensor

--- basic/basic.ino
+++ basic/basic.ino
@@ -1,16 +1,25 @@
 #include <Arduino.h>
 #include <SparkFun_TB6612.h>
 
-// US initializations
-#define ECHO 8 // echo value back 
-#define TRIG 9 // triggers sound output
-int duration; // duration it takes for pulse echo off object and come back
-float distance; // distance to object
+// US1 initializations
+#define ECHO1 8 // echo value back 
+#define TRIG1 9 // triggers sound output
+int duration1; // duration it takes for pulse echo off object and come back
+float distance1; // distance to object
 
-// IR initializations
-#define IR_LED 13 // LED on IR sensor
-#define IR_INPUT 7 // input pin for IR sensor
-int IR_isObstacle = HIGH; // for IR sensor, high means yes
+// IR1 initializations
+#define IR_INPUT1 7 // input pin for IR sensor
+int IR_isObstacle1 = HIGH; // for IR sensor, high means yes
+
+// US2 initializations
+#define ECHO2 12
+#define TRIG2 13
+int duration2;
+int distance2;
+
+// IR2 initializations
+#define IR_INPUT2 14
+int IR_isObstacle2 = HIGH;
 
 // motor initializations
 #define AIN1 2
@@ -30,44 +39,58 @@ void setup() {
   Serial.begin(9600);
 
   // initialize IR
-  pinMode(IR_INPUT, INPUT);
-  pinMode(IR_LED, OUTPUT);
+  pinMode(IR_INPUT1, INPUT);
+  pinMode(IR_INPUT2, INPUT);
 
   // initialize US
-  pinMode(TRIG, OUTPUT);
-  pinMode(ECHO, INPUT);
+  pinMode(TRIG1, OUTPUT);
+  pinMode(ECHO1, INPUT);
+  pinMode(TRIG2, OUTPUT);
+  pinMode(ECHO2, INPUT);
 
   // ensure trigger is set to low at first
-  digitalWrite(TRIG, LOW);
+  digitalWrite(TRIG1, LOW);
+  digitalWrite(TRIG2, LOW);
 
 }
 
 void loop() {
 
-  // IR
-  IR_isObstacle = digitalRead(IR_INPUT);
+  // IR1
+  IR_isObstacle1 = digitalRead(IR_INPUT1);
 
-  if (IR_isObstacle == LOW)
+  if (IR_isObstacle1 == LOW)
   {
-    Serial.println("White surface.");
-    digitalWrite(IR_LED, HIGH);
+    Serial.println("White surface 1.");
   }
   else
   {
-    Serial.println("Black surface.");
-    digitalWrite(IR_LED, LOW);
+    Serial.println("Black surface 1.");
   }
 
   delay(200);
 
-  // US
-   
-  digitalWrite(TRIG, HIGH); // pulse trigger
-  delayMicroseconds(10); 
-  digitalWrite(TRIG, LOW);
-  duration = pulseIn(ECHO, HIGH); // read echo
+  // IR2
+  IR_isObstacle2 = digitalRead(IR_INPUT2);
 
-  if (duration >= 38000) {
+  if (IR_isObstacle2 == LOW)
+  {
+    Serial.println("White surface 2.");
+  }
+  else
+  {
+    Serial.println("Black surface 2.");
+  }
+
+  delay(200);
+
+  // US1
+  digitalWrite(TRIG1, HIGH); // pulse trigger
+  delayMicroseconds(10); 
+  digitalWrite(TRIG1, LOW);
+  duration1 = pulseIn(ECHO1, HIGH); // read echo
+
+  if (duration1 >= 38000) {
 
     Serial.print("Out of range"); 
 
@@ -75,12 +98,43 @@ void loop() {
   else
   {
     // calculate distance to object
-    distance = duration / 58; 
-    Serial.print(distance); 
+    distance1 = duration1 / 58; 
+    Serial.print(distance1); 
     Serial.println(" cm");
 
   }
 
   delay(1000); 
+
+
+  // US2
+  digitalWrite(TRIG2, HIGH); // pulse trigger
+  delayMicroseconds(10); 
+  digitalWrite(TRIG2, LOW);
+  duration2 = pulseIn(ECHO2, HIGH); // read echo
+
+  if (duration2 >= 38000) {
+
+    Serial.print("Out of range"); 
+
+  }
+  else
+  {
+    // calculate distance to object
+    distance2 = duration2 / 58; 
+    Serial.print(distance2); 
+    Serial.println(" cm");
+
+  }
+
+  delay(1000); 
+
+  
+  left_motor.drive(100, 1000);
+  right_motor.drive(100, 1000);
+  
+
+  delay(1000);
+
 
 }
